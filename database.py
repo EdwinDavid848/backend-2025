a972127ae626bb1c7ef3204587b4351a6df4e423
daniela707-sys <143579120+daniela707-sys@users.noreply.github.com>
2025-04-16 12:54:17 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 #DATABASE_URL = "mysql+mysqlconnector://root:Mondangi707@localhost:3306/prueba_cuatro"
-DATABASE_URL = 'mysql+mysqlconnector://admin_seven:unknown-707@127.0.0.1:3306/db'
+#DATABASE_URL = 'mysql+mysqlconnector://admin_seven:unknown-707@127.0.0.1:3306/db'
+DATABASE_URL = "mysql+mysqlconnector://root:iImPBxAlLBXBfwTYmYwSPoxVLbAfdrwy@mainline.proxy.rlwy.net:17609/dbRailway"
+#DATABASE_URL = "mysql+mysqlconnector://root:iImPBxAlLBXBfwTYmYwSPoxVLbAfdrwy@mysql.railway.internal:3306/dbRailway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
